a77b8029486f105943d40f010dc3c85073b0f100
Johan Garzon <johanprograma@hotmail.com>
2020-11-04 16:10:06 +0100

    Div appears on navbar when log in succeded. Use of token.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,23 +8,34 @@ export default function Navbar() {
   console.log('token', token)
 
   return (
-    <nav className='navbar navbar-expand-md navbar-light bg-light'>
-      <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+    <nav className='navbar navbar-expand-sm navbar-light bg-light'>
+      <button className='navbar-toggler' 
+              type='button' 
+              data-toggle='collapse' 
+              data-target='#navbarSupportedContent' 
+              aria-controls='navbarSupportedContent' 
+              aria-expanded='false' 
+              aria-label='Toggle navigation'>
         <span className='navbar-toggler-icon'></span>
       </button>
+
+      {/* Here comes the links */}
       <div className='collapse navbar-collapse' id='navbarSupportedContent'>
         <ul className='navbar-nav mr-auto'>
+
           <li className='nav-item'>
             <Link to='/' className='nav-link'>Login</Link>
           </li>
+
           <li className='nav-item'>
             <Link to='/home' className='nav-link'>Home</Link>
           </li>
+          
         </ul>
         {
           token && 
           <div className='nav-item'>
-            <span className='nav-link'>I´m logged in</span>
+            <span className='nav-item'>Loggen in! (token)</span>
           </div>
         }
       </div>
