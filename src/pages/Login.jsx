af5f74b79b53450c22c97d940f42609400af91c7
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-11-04 12:17:15 +0100

    User can now login and retrieve a token

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {TokenContext} from "../contexts/TokenContext";
 import User from "../data/User";
 
@@ -7,11 +7,16 @@ const Login = () => {
     const [password, setPassword] = useState('');
     const {token, setToken} = useContext(TokenContext);
 
-    async function loginUser() {
+    async function loginUser(event) {
+        event.preventDefault();
         const response = await User.getToken(email, password);
         const data = await response.json();
-        setToken(data.token)
+        setToken(data.token);
     }
+
+    useEffect(() => {
+        console.log(token);
+    },[token])
 
     return (
         <div className='row justify-content-md-center'>
@@ -20,13 +25,15 @@ const Login = () => {
                     <div className="form-group">
                         <label htmlFor="exampleInputEmail1">Email address</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                               placeholder="Enter email"/>
+                               placeholder="Enter email"
+                               onChange={e => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" placeholder="Password"/>
+                        <input type="password" className="form-control" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary" onClick={loginUser}>Login</button>
                 </form>
             </div>
         </div>
