1062800c90f4ab63ac936422023cef2ae6e67455
Johan Garzon <johanprograma@hotmail.com>
2020-11-06 16:11:51 +0100

    All customer information is rendered in CustomerSingle

--- src/components/CustomerSingle.jsx
+++ src/components/CustomerSingle.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function CustomerSingle(props) {
+  const { customer } = props
   return (
     <div>
-      Here comes the customer's name {props.customer.name}
+      <h1> { customer.name } </h1>
+      <div> { customer.organisationNr } </div>
+      <div> { customer.vatNr } </div>
+      <div> { customer.reference } </div>
+      <div> { customer.paymentTerm } </div>
+      <div> { customer.website } </div>
+      <div> { customer.email } </div>
+      <div> { customer.phoneNumber } </div>
     </div>
   )
 }
