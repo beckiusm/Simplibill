80e420aff1db78503ac32c1febdb6d4e19af260e
Mathias Beckius <lillmatte@hotmail.com>
2020-11-09 18:15:21 +0100

    added some luft on the startsida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import CustomerList from '../components/CustomerList';
 const Home = () => {
     return (
         <div>
-            <h1>Your customers</h1>
+            <h1 className="mt-3 mb-3">Your customers</h1>
             <CustomerList/>
         </div>
     );
