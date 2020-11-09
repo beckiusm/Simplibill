4bc6a03a8aa2344d50cd1ec822312783e6c904fe
Johan Garzon <johanprograma@hotmail.com>
2020-11-09 16:15:17 +0100

    Move link AddCustomer to Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,10 @@ import {Link} from "react-router-dom";
 import CustomerList from '../components/CustomerList';
 
 const Home = () => {
-    /**
-     * Temp!
-     */
-
     return (
         <div>
             <h1>Home</h1>
             <CustomerList/>
-            <ul>
-                <li><Link to='/addCustomer'>Lägg till ny kund</Link></li>
-            </ul>
         </div>
     );
 };
